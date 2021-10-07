0998ddc7733252bf06a1fbcd97945d921f5a4c67
kami1983 <meetcancanyou@yahoo.com>
2021-10-07 09:48:32 -0400

    Add reveal.

--- pallets/atocha/src/lib.rs
+++ pallets/atocha/src/lib.rs
@@ -8,170 +8,240 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-
 #[frame_support::pallet]
 pub mod pallet {
 
-	pub type PuzzleRelationType = u8;
-	pub type PuzzleStatus = u8;
-	pub type PuzzleSubjectHash = Vec<u8>;
-	pub type PuzzleAnswerHash = Vec<u8>;
-	pub type PuzzleTicket = u64;
+    pub type PuzzleRelationType = u8;
+    pub type PuzzleStatus = u8;
+    pub type PuzzleSubjectHash = Vec<u8>;
+    pub type PuzzleAnswerHash = Vec<u8>;
+    pub type PuzzleAnswerOption = Option<PuzzleAnswerHash>;
+    pub type PuzzleTicket = u64;
 
-	// 1=solving, 2=up to time, 3=solve
-	pub const PUZZLE_STATUS_IS_SOLVING: PuzzleStatus = 1;
-	pub const PUZZLE_STATUS_IS_UP_TO_TIME: PuzzleStatus = 2;
-	pub const PUZZLE_STATUS_IS_SOLVED: PuzzleStatus = 3;
+    pub type PuzzleAnswerSigned = Vec<u8>;
+    pub type PuzzleAnswerNonce = Vec<u8>;
+    pub type CreateBn = u64;
+    pub type DurationBn = u64;
+    pub type RevealBn = u64;
+    pub type PuzzleVersion = u64;
 
+    pub type PuzzleAnswerStatus = u8;
 
-	// 引入需要的包
-	use frame_support::{
-		dispatch::DispatchResultWithPostInfo,
-		pallet_prelude::*,
-	};
-	// 比较粗暴的引用 frame_system 所有宏函数，和系统类型信息
-	use frame_system::pallet_prelude::*;
-	use sp_std::vec::Vec;
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
+    // 1=solving, 2=up to time, 3=solve
+    pub const PUZZLE_STATUS_IS_SOLVING: PuzzleStatus = 1;
+    pub const PUZZLE_STATUS_IS_UP_TO_TIME: PuzzleStatus = 2;
+    pub const PUZZLE_STATUS_IS_SOLVED: PuzzleStatus = 3;
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub (super) trait Store)]
-	pub struct Pallet<T>(_);
+    // 引入需要的包
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    // 比较粗暴的引用 frame_system 所有宏函数，和系统类型信息
+    use frame_system::pallet_prelude::*;
+    use sp_std::vec::Vec;
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
 
-	#[pallet::storage]
-	#[pallet::getter(fn my_puzzle)]
-	pub type Puzzle<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		Vec<u8>,
-		( T::AccountId, Vec<u8> )
-	>;
+    #[pallet::pallet]
+    #[pallet::generate_store(pub (super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	#[pallet::storage]
-	#[pallet::getter(fn puzzle_relation)]
-	pub type PuzzleRelation<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		T::AccountId,
-		// puzzle_hash, answer_hash, ticket (Balance type), relation_type (1=Creater, 2=Answer), status (1=solving, 2=up to time, 3=solve), create_bn, expired_bn
-		Vec<(PuzzleSubjectHash, PuzzleAnswerHash, PuzzleTicket, PuzzleRelationType, PuzzleStatus, u64, u64 )>,
-	>;
+    // #[pallet::storage]
+    // #[pallet::getter(fn my_puzzle)]
+    // pub type Puzzle<T: Config> = StorageMap<
+    // 	_,
+    // 	Blake2_128Concat,
+    // 	Vec<u8>,
+    // 	( T::AccountId, Vec<u8> )
+    // >;
 
-	#[pallet::event]
-	// Make a metadata, used by WebUI
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	// Make a help methods, used by the caller
-	#[pallet::generate_deposit(pub (super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		// creator id, puzzle_hash, create block number , duration block number,
-		PuzzleCreated(T::AccountId, PuzzleSubjectHash, u64, u64),
-		PuzzleRevoked(T::AccountId, Vec<u8>),
-	}
+    #[pallet::storage]
+    #[pallet::getter(fn puzzle_info)]
+    pub type PuzzleInfo<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        PuzzleSubjectHash,
+        (
+            T::AccountId,
+            PuzzleAnswerOption,
+            PuzzleAnswerSigned,
+            PuzzleAnswerNonce,
+            PuzzleTicket,
+            PuzzleStatus,
+            CreateBn,
+            DurationBn,
+            RevealBn,
+            PuzzleVersion,
+        ),
+    >;
 
+    #[pallet::storage]
+    #[pallet::getter(fn puzzle_direct_answer)]
+    pub type PuzzleDirectAnswer<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        PuzzleSubjectHash,
+        // puzzle_hash, answer_hash, ticket (Balance type), relation_type (1=Creater, 2=Answer), status (1=solving, 2=up to time, 3=solve), create_bn, expired_bn
+        Vec<(
+            T::AccountId,
+            PuzzleAnswerHash,
+            PuzzleTicket,
+            PuzzleAnswerStatus,
+            CreateBn,
+        )>,
+    >;
 
-	#[pallet::error]
-	pub enum Error<T> {
-		// 定义一个错误信息，存证已经存在
-		ProofAlreadyExist,
-		ClaimNotExist,
-		NotClaimOwner,
-		PuzzleAlreadyExist,
-		PuzzleNotExist,
-		NotPuzzleOwner,
-	}
+    #[pallet::event]
+    // Make a metadata, used by WebUI
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    // Make a help methods, used by the caller
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        // creator id, puzzle_hash, create block number , duration block number,
+        PuzzleCreated(T::AccountId, PuzzleSubjectHash, CreateBn, DurationBn),
+        AnswerCreated(T::AccountId, PuzzleAnswerHash, PuzzleSubjectHash, CreateBn),
+        PuzzleRevoked(T::AccountId, Vec<u8>),
+    }
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    #[pallet::error]
+    pub enum Error<T> {
+        // 定义一个错误信息，存证已经存在
+        ProofAlreadyExist,
+        ClaimNotExist,
+        NotClaimOwner,
+        PuzzleAlreadyExist,
+        AnswerAlreadyExist,
+        PuzzleNotExist,
+        NotPuzzleOwner,
+    }
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T>
-		where u64: From<<T as frame_system::Config>::BlockNumber>
-	{
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-		// {
-		// "puzzle_hash": "QmYiqDpdbkekTsz1dFsgd5jpVcGqEmN6nmGuJ1tdCmJApQ",
-		// "answer_hash": "QmZvMUwrciwAgCrEpesLiiWz41TaMTsqzZF1Z74sLj6pFU",
-		// "relation_type": 1,
-		// "account_id": "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
-		// }
+    #[pallet::call]
+    impl<T: Config> Pallet<T>
+    where
+        u64: From<<T as frame_system::Config>::BlockNumber>,
+    {
+        #[pallet::weight(1234)]
+        pub fn create_puzzle(
+            origin: OriginFor<T>,
+            puzzle_hash: PuzzleSubjectHash,
+            answer_signed: PuzzleAnswerSigned,
+            answer_nonce: PuzzleAnswerNonce,
+            ticket: PuzzleTicket,
+            duration: DurationBn,
+            puzzle_version: PuzzleVersion,
+        ) -> DispatchResultWithPostInfo {
+            // check signer
+            let who = ensure_signed(origin)?;
+            //
+            let current_block_number = <frame_system::Pallet<T>>::block_number();
+            let dration_block_number = duration.checked_add(current_block_number.into()).unwrap();
 
-		// (PuzzleSubjectHash,
-		// PuzzleAnswerHash,
-		// PuzzleTicket,
-		// PuzzleRelationType,
-		// PuzzleStatus,
-		// T::BlockNumber,
-		// T::BlockNumber )
-		#[pallet::weight(1234)]
-		pub fn create_puzzle(
-			origin: OriginFor<T>,
-			puzzle_owner: T::AccountId,
-			puzzle_hash: PuzzleSubjectHash,
-			answer_hash: PuzzleAnswerHash,
-			ticket: PuzzleTicket,
-			// relation type to identify the puzzle relation who create puzzle who answer puzzle.
-			relation_type: PuzzleRelationType,
-			duration: u64,
-		) -> DispatchResultWithPostInfo { // 返回值是一个Result类型的别名它会包含一些weight的信息，这是通过use引入进来的
-			// check signer
-			let who = ensure_signed(origin)?;
+            ensure!(
+                !<PuzzleInfo<T>>::contains_key(&puzzle_hash),
+                Error::<T>::PuzzleAlreadyExist
+            );
 
-			// check current origin is creator or answer
-			let mut relation_type = 2;
-			if who == puzzle_owner {
-				relation_type = 1;
-			}
+            type PuzzleContent<T> = (
+                <T as frame_system::Config>::AccountId,
+                PuzzleAnswerOption,
+                PuzzleAnswerSigned,
+                PuzzleAnswerNonce,
+                PuzzleTicket,
+                PuzzleStatus,
+                CreateBn,
+                DurationBn,
+                RevealBn,
+                PuzzleVersion,
+            );
 
-			//
-			let current_block_number = <frame_system::Pallet<T>>::block_number();
-			let dration_block_number =  duration.checked_add(current_block_number.into()).unwrap();
+            let puzzle_content: PuzzleContent<T> = (
+                who.clone(),
+                None,
+                answer_signed.clone(),
+                answer_nonce.clone(),
+                ticket,
+                PUZZLE_STATUS_IS_SOLVING,
+                current_block_number.clone().into(),
+                dration_block_number.clone(),
+                0,
+                1,
+            );
+            <PuzzleInfo<T>>::insert(puzzle_hash.clone(), puzzle_content);
 
-			type PuzzleRealtionContent = Vec<(PuzzleSubjectHash, PuzzleAnswerHash, PuzzleTicket, PuzzleRelationType, PuzzleStatus, u64, u64)>;
+            // send event
+            Self::deposit_event(Event::PuzzleCreated(
+                who,
+                puzzle_hash,
+                current_block_number.into(),
+                dration_block_number,
+            ));
+            //
+            Ok(().into())
+        }
 
-			// check owner has exists
- 			if <PuzzleRelation<T>>::contains_key(&puzzle_owner) {
-				// check puzzle_hash has exists
-				let mut puzzle_relation_vec: PuzzleRealtionContent = <PuzzleRelation<T>>::get(&puzzle_owner).unwrap();
-				let puzzle_exists = puzzle_relation_vec.iter().any(|(old_puzzle_hash, _, _, _, _, _, _,)| { old_puzzle_hash == &puzzle_hash });
-				// tip exists and break call.
-				ensure!(!puzzle_exists, Error::<T>::PuzzleAlreadyExist);
-				// add puzzle Vec<(PuzzleSubjectHash, PuzzleAnswerHash, PuzzleTicket, PuzzleRelationType, PuzzleStatus, T::BlockNumber, T::BlockNumber )>
-				puzzle_relation_vec.push((puzzle_hash.clone(), answer_hash, ticket, relation_type, PUZZLE_STATUS_IS_SOLVING, current_block_number.into(), dration_block_number ));
-				<PuzzleRelation<T>>::insert(&puzzle_owner, puzzle_relation_vec);
-			} else {
-				// create puzzle
-				let mut puzzle_relation_vec: PuzzleRealtionContent = Vec::new();
-				puzzle_relation_vec.push((puzzle_hash.clone(), answer_hash, ticket, relation_type, PUZZLE_STATUS_IS_SOLVING, current_block_number.into(), dration_block_number ));
-				<PuzzleRelation<T>>::insert(&puzzle_owner, puzzle_relation_vec);
-			}
+        #[pallet::weight(1234)]
+        pub fn answer_puzzle(
+            origin: OriginFor<T>,
+            puzzle_hash: PuzzleSubjectHash,
+            answer_hash: PuzzleAnswerHash,
+            ticket: PuzzleTicket,
+        ) -> DispatchResultWithPostInfo {
+            // check signer
+            let who = ensure_signed(origin)?;
 
-			// send event
-			Self::deposit_event(Event::PuzzleCreated(who, puzzle_hash, current_block_number.into(), dration_block_number));
-			//
-			Ok(().into())
-		}
+            //
+            let current_block_number = <frame_system::Pallet<T>>::block_number();
 
-		// 删除一个谜题 测试
-		#[pallet::weight(1235)]
-		pub fn revoke_puzzle(
-			origin: OriginFor<T>,
-			puzzle_hash: Vec<u8>,
-		) -> DispatchResultWithPostInfo {
-			// 先验证 origin
-			let sender = ensure_signed(origin)?;
-			let (owner, _) = Puzzle::<T>::get(&puzzle_hash).ok_or(Error::<T>::PuzzleNotExist)?;
-			// 判断发送者和存证所有者是否是同一个人
-			ensure!(owner == sender, Error::<T>::NotPuzzleOwner);
-			// 删除存储的puzzle
-			Puzzle::<T>::remove(&puzzle_hash);
-			// 发送存证Revoked事件
-			Self::deposit_event(Event::PuzzleRevoked(sender, puzzle_hash));
-			// 返回函数成功结果
-			Ok(().into())
-		}
+            //(T::AccountId, PuzzleAnswerHash, PuzzleTicket, PuzzleAnswerStatus, CreateBn)
+            type AnswerContent<T> = (
+                <T as frame_system::Config>::AccountId,
+                PuzzleAnswerHash,
+                PuzzleTicket,
+                PuzzleAnswerStatus,
+                CreateBn,
+            );
 
-	}
+            // Puzzle need exists.
+            ensure!(
+                <PuzzleInfo<T>>::contains_key(&puzzle_hash),
+                Error::<T>::PuzzleNotExist
+            );
+
+            let mut answer_store_list: Vec<AnswerContent<T>> = Vec::new();
+            let answer_list_opt = <PuzzleDirectAnswer<T>>::get(&puzzle_hash);
+            if let Some(answer_list) = answer_list_opt {
+                // Determine whether the answer already exists.
+                let answer_exists = answer_list
+                    .iter()
+                    .any(|(_, old_answer_hash, _, _, _)| &answer_hash == old_answer_hash);
+                ensure!(!answer_exists, Error::<T>::AnswerAlreadyExist);
+                answer_store_list = answer_list;
+            }
+
+            // create new answer tuple.
+            let answer_content: AnswerContent<T> = (
+                who.clone(),
+                answer_hash.clone(),
+                ticket,
+                0, // 1=solving ???, 2=NONE, 3=solved ????4=closed ???
+                current_block_number.clone().into(),
+            );
+            answer_store_list.push(answer_content);
+
+            <PuzzleDirectAnswer<T>>::insert(puzzle_hash.clone(), answer_store_list);
+
+            // send event
+            Self::deposit_event(Event::AnswerCreated(
+                who,
+                answer_hash,
+                puzzle_hash,
+                current_block_number.into(),
+            ));
+            //
+            Ok(().into())
+        }
+    }
 }
--- pallets/atocha/src/tests.rs
+++ pallets/atocha/src/tests.rs
@@ -1,44 +1,211 @@
-use crate::{Error, mock::*};
-use crate::pallet::{*};
 use super::Event as AtochaEvent;
-use frame_support::{assert_ok, assert_noop};
+use crate::pallet::*;
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
 
 const CONST_ORIGIN_IS_CREATOR: u64 = 1;
+const CONST_ORIGIN_IS_ANSWER_1: u64 = 2;
+const CONST_ORIGIN_IS_ANSWER_2: u64 = 3;
+const CONST_ORIGIN_IS_ANSWER_3: u64 = 4;
 
 #[test]
-fn it_works_for_default_value() {
+fn test_create_puzzle() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(5);
 
-	new_test_ext().execute_with(|| {
-		System::set_block_number(5);
+        handle_create_puzzle(
+            CONST_ORIGIN_IS_CREATOR,
+            "PUZZLE_HASH",
+            "ANSWER_SIGNED",
+            "NONCE",
+            10,
+            50,
+        );
 
-		let origin = Origin::signed(CONST_ORIGIN_IS_CREATOR);
-		let puzzle_owner = CONST_ORIGIN_IS_CREATOR ; // The same as origin, so it is a create puzzle operation.
-		let puzzle_hash = "PUZZLE_HASH".as_bytes().to_vec();
-		let answer_hash = "ANSWER_HASH".as_bytes().to_vec();
-		let ticket: PuzzleTicket = 10;
-		let relation_type: PuzzleRelationType = 1;
-		let duration: u64 = 50;
+        // (PuzzleSubjectHash, PuzzleAnswerHash, PuzzleTicket, PuzzleRelationType, PuzzleStatus, u64, u64 )
+        let relation_info = AtochaModule::puzzle_info(toVec("PUZZLE_HASH")).unwrap();
+        // println!("==== {:?}", relation_info);
+        // if let (_, _, puzzle_hash, _, _, _, _, _, _, _) = relation_info {
+        //     println!("puzzle_hash = {:?}", sp_std::str::from_utf8(&puzzle_hash));
+        // };
 
-		// Dispatch a signed extrinsic.
-		assert_ok!(AtochaModule::create_puzzle(
-			origin,
-			puzzle_owner.clone(),
-			puzzle_hash.clone(),
-			answer_hash.clone(),
-			ticket.clone(),
-			relation_type.clone(),
-			duration.clone()
-		));
-
-		// (PuzzleSubjectHash, PuzzleAnswerHash, PuzzleTicket, PuzzleRelationType, PuzzleStatus, u64, u64 )
-		let relation_info_vec = AtochaModule::puzzle_relation(CONST_ORIGIN_IS_CREATOR).unwrap();
-		println!("==== {:?}", relation_info_vec);
-		assert_eq!(relation_info_vec[0], (puzzle_hash.clone(), answer_hash.clone(), ticket, relation_type, PUZZLE_STATUS_IS_SOLVING, 5, 5 + duration));
-
-		System::assert_last_event(AtochaEvent::PuzzleCreated(CONST_ORIGIN_IS_CREATOR, puzzle_hash, 5, 5 + duration).into());
-	});
+        assert_eq!(
+            relation_info,
+            (
+                CONST_ORIGIN_IS_CREATOR,
+                None,
+                toVec("ANSWER_SIGNED"), //.as_bytes().to_vec(),
+                toVec("NONCE"),         //.as_bytes().to_vec(),
+                10,
+                PUZZLE_STATUS_IS_SOLVING,
+                5,
+                5 + 50,
+                0,
+                1,
+            )
+        );
+        //
+        System::assert_last_event(
+            AtochaEvent::PuzzleCreated(
+                CONST_ORIGIN_IS_CREATOR,
+                toVec("PUZZLE_HASH"), //.as_bytes().to_vec(),
+                5,
+                5 + 50,
+            )
+            .into(),
+        );
+    });
 }
 
+#[test]
+fn test_answer_puzzle() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(5);
+
+        // check initial status.
+        let answer_list = AtochaModule::puzzle_direct_answer(&toVec("PUZZLE_HASH"));
+        assert_eq!(None, answer_list);
+
+        // if puzzle not exists.
+        assert_noop!(
+            // Try to call create answer, but the puzzle not exists.
+            AtochaModule::answer_puzzle(
+                Origin::signed(CONST_ORIGIN_IS_ANSWER_1),
+                toVec("PUZZLE_HASH"),
+                toVec("ANSWER_HASH"),
+                500,
+            ),
+            Error::<Test>::PuzzleNotExist
+        );
+
+        // Create puzzle hash on the chain.
+        handle_create_puzzle(
+            CONST_ORIGIN_IS_CREATOR,
+            "PUZZLE_HASH",
+            "ANSWER_SIGNED",
+            "NONCE",
+            10,
+            50,
+        );
+
+        // add answer
+        // origin: OriginFor<T>,
+        // puzzle_hash: PuzzleSubjectHash,
+        // answer_hash: PuzzleAnswerHash,
+        // ticket: PuzzleTicket,
+        assert_ok!(AtochaModule::answer_puzzle(
+            Origin::signed(CONST_ORIGIN_IS_ANSWER_1),
+            toVec("PUZZLE_HASH"),
+            toVec("ANSWER_HASH"),
+            500,
+        ));
+
+        // check answer list count.
+        let answer_list = AtochaModule::puzzle_direct_answer(&toVec("PUZZLE_HASH"));
+        assert!(answer_list.is_some());
+        if let Some(answer_list) = answer_list {
+            // println!("answer_list = {:?}", answer_list);
+            assert_eq!(1, answer_list.len());
+            // check list item
+            // Vec<(
+            //     T::AccountId,
+            //     PuzzleAnswerHash,
+            //     PuzzleTicket,
+            //     PuzzleAnswerStatus,
+            //     CreateBn,
+            // )>,
+            assert_eq!(
+                (CONST_ORIGIN_IS_ANSWER_1, toVec("ANSWER_HASH"), 500, 0, 5),
+                answer_list[0]
+            );
+        }
+    });
+}
+
+#[test]
+fn test_reveal_puzzle() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(5);
+        //
+        use sp_application_crypto::sr25519;
+        use sp_runtime::MultiSignature;
+        use sp_runtime::MultiSigner;
+
+        // sp_core::sr25519::Pair(schnorrkel::Keypair).;
+
+        // let result = AuthorityPair::verify(signature.into(), signature.into(), test_address.into());
+        // assert!(result, "Result is true.")
+
+        let msg = &b"test-message"[..];
+        let (pair, _) = sr25519::Pair::generate();
+
+        let signature = pair.sign(&msg);
+        assert!(sr25519::Pair::verify(&signature, msg, &pair.public()));
+
+        println!("msg = {:?}", &msg);
+        println!("signature = {:?}", &signature);
+        println!("pair.public() = {:?}", &pair.public());
+        // println!("multi_signer.into_account() = {:?}", &multi_signer.into_account());
+
+
+        let multi_sig = MultiSignature::from(signature); // OK
+        let multi_signer = MultiSigner::from(pair.public());
+        assert!(multi_sig.verify(msg, &multi_signer.into_account()));
+
+        let multi_signer = MultiSigner::from(pair.public());
+        assert!(multi_sig.verify(msg, &multi_signer.into_account()));
+
+        //---------
+
+
+        let test_signature = &hex::decode("2aeaa98e26062cf65161c68c5cb7aa31ca050cb5bdd07abc80a475d2a2eebc7b7a9c9546fbdff971b29419ddd9982bf4148c81a49df550154e1674a6b58bac84").expect("Hex invalid")[..];
+        let signature = Signature::try_from(test_signature);
+        let signature = signature.unwrap();
+        println!(" signature = {:?}", signature);
+        let account_result =  AccountId::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty");
+
+        let account_id = account_result.unwrap();
+        println!(" account_id = {:?} ", account_id);
+        let public_id = Public::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty");
+        let public_id = public_id.unwrap();
+        println!(" public_id = {:?} ", public_id);
+
+        let multi_sig = MultiSignature::from(signature); // OK
+        let multi_signer = MultiSigner::from(public_id);
+        assert!(multi_sig.verify("This is a text message".as_bytes(), &multi_signer.into_account()));
+
+    });
+}
+
+fn handle_create_puzzle(
+    account_id: u64,
+    puzzle_hash: &str,
+    answer_signed: &str,
+    answer_nonce: &str,
+    ticket: PuzzleTicket,
+    duration: DurationBn,
+) {
+    let origin = Origin::signed(account_id);
+    let puzzle_hash = puzzle_hash.as_bytes().to_vec();
+    let answer_signed = answer_signed.as_bytes().to_vec();
+    let answer_nonce = answer_nonce.as_bytes().to_vec();
+    let puzzle_version: PuzzleVersion = 1;
+
+    // Dispatch a signed extrinsic.
+    assert_ok!(AtochaModule::create_puzzle(
+        origin,
+        puzzle_hash.clone(),
+        answer_signed.clone(),
+        answer_nonce.clone(),
+        ticket.clone(),
+        duration.clone(),
+        puzzle_version.clone()
+    ));
+}
+
+fn toVec(to_str: &str) -> Vec<u8> {
+    to_str.as_bytes().to_vec()
+}
 
 // #[test]
 // fn correct_error_for_none_value() {
